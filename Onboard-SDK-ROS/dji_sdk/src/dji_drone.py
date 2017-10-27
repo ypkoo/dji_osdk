64e2559ceedb82444d0fcb7de91ff268a1b3e439
ypkoo <ypkoo93@gmail.com>
2017-10-28 00:41:47 +0900

    initial commit

---
+++ Onboard-SDK-ROS/dji_sdk/src/dji_drone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#encoding = utf-8
+
+import rospy
+import actionlib
+import roslib
+import dji_sdk.msg
+import dji_sdk.srv
+
+class Drone(object):
+
+	def flight_control_callback(self):
+		pass
